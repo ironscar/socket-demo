7d6188b903e32497a6b5fac25612355e4dfa7e17
upamanyu <uchakravarty@ti.com>
2022-12-29 08:49:17 +0530

    refactor: get session id

--- src/main/java/com/example/socketdemo/web/MessageController.java
+++ src/main/java/com/example/socketdemo/web/MessageController.java
@@ -2,6 +2,7 @@ package com.example.socketdemo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.support.MessageBuilder;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import com.example.socketdemo.domain.Greeting;
 import com.example.socketdemo.domain.Hello;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class MessageController {
 
@@ -17,7 +21,8 @@ public class MessageController {
     private SimpMessagingTemplate template;
 
     @MessageMapping("/hello")
-    public void greeting(Hello msg) throws InterruptedException {
+    public void greeting(@Header("simpSessionId") String sessionId, Hello msg) throws InterruptedException {
+        log.info("session id = " + sessionId);
         Thread.sleep(2000);
         Message<Greeting> response = MessageBuilder.withPayload(new Greeting("Hi, " + msg.getName())).build();
         template.convertAndSend("/topic/greetings", response);
@@ -27,3 +32,12 @@ public class MessageController {
     }
 
 }
+
+/**
+ * If there are multiple instances of this app
+ * And if a socket is established
+ * And I start an async long-running job that keeps sending message to client
+ * And then client sends a STOP message to the same socket
+ * Does it mean that the client will definitely hit the same instance and so I can manage it in app-memory
+ * Or do I need to assume that the job may be running in a different instance
+ */
